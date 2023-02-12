89a1d3f4159bbf8b2c1ed5bdd79a410fa8be2a9a
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-12 23:16:22 +0530

    12 feb

---
+++ printlnVSprint.java
@@ -0,0 +1,17 @@
+public class printlnVSprint
+{
+    public static void main(String []args)
+    {
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("");
+                                   
+        System.out.print("*");
+        System.out.print("");
+        System.out.print("*");
+        System.out.print("");
+        System.out.print("*");
+    }
+    
+}
