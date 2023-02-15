6bd765cdc55a38e6c3e6e86510c5d9fd9f9a7344
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-15 23:46:31 +0530

    first

--- CreatArray.java
+++ CreatArray.java
@@ -2,7 +2,7 @@ public class CreatArray
 {
     public static void main(String []args)
     {
-        int[] a = {1,2,3,4};
+        int[] a = {0,2,3,4};
         
         for(int i=0;i<=3;i++)
         {
