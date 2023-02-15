f5e78a9c8b35865b3cf26e19ddc720845feeb871
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-15 23:46:55 +0530

    first

--- Pattern4.java
+++ Pattern4.java
@@ -2,7 +2,7 @@ public class Pattern4
 {
     public static void main(String []args)
     {
-        int n=5;
+        int n=8;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
