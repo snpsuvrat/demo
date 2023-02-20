0600939b79f6813706aa23cedb1e1aec3eab6021
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-20 17:05:48 +0530

    hello

---
+++ launch.java
@@ -0,0 +1,12 @@
+public class launch
+{  
+    public static void main(String[] args) 
+    {
+        String s1="PWskills";
+        String s2=new String ("PWskills");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        
+    }
+    
+}
