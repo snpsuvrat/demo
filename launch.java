56209c6f3869cd8d7dde64c9e61773670212c1da
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-20 17:14:15 +0530

    hello

--- launch.java
+++ launch.java
@@ -2,10 +2,17 @@ public class launch
 {  
     public static void main(String[] args) 
     {
+        // String s1="PWskills";
+        // String s2=new String ("PWskills");
+        // System.out.println(s1==s2);
+        // System.out.println(s1.equals(s2));
         String s1="PWskills";
-        String s2=new String ("PWskills");
+        String s2="PWskills";
+        String s3="PWSKILLS";
         System.out.println(s1==s2);
         System.out.println(s1.equals(s2));
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s3));
         
     }
     
