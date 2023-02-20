dfc8d57480aea3d7cb1f4059514ea90f2cd79fa8
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-20 18:16:32 +0530

    hello

---
+++ Example.java
@@ -0,0 +1,12 @@
+public class Example 
+{ public static void main(String[] args) 
+{    
+    String s="Sachin";
+    System.out.println(s.concat("Tendulakr"));
+    s+="IND" ;
+    s=s+" MI";
+    System.out.println(s);
+
+}
+    
+}
