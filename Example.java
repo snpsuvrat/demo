b06dbf8130aca0699ab57bc89160c99b0aa8a912
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-02-25 15:43:05 +0530

    heello

--- Example.java
+++ Example.java
@@ -1,9 +1,11 @@
 public class Example 
 { public static void main(String[] args) 
 {    
-    String s1="hello";
-    System.out.println(s1.length());
+    String fName = "James";
 
+String lName = "Gosling"; 
+
+System.out.println(fName = lName);
 }
     
 }
--- Test.java
+++ Test.java
@@ -1,10 +1,17 @@
-public class Test{
+public class Test {
 
-    public static void main(String args[]){
-   
-        int[] arr= new int[5];
-
-        arr = new int[6];
-   }
-   
-   }
+    public static void main(String[] args) {
+    
+    StringBuilder sb = new StringBuilder("Good"); //Line 3 change(sb); //Line 4
+    
+    System.out.println(sb); //Line 5
+    
+    }
+    
+    private static void change(StringBuilder s) { 
+    
+    s.append("_Morning"); //Line 9
+    
+    }
+    
+    }
