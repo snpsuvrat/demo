a0c3e7c04f88fdfa76adf0a3a091ffbf0afba89f
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-03-03 23:39:20 +0530

    hllo

---
+++ MutableString2.java
@@ -0,0 +1,35 @@
+public class MutableString2 
+{
+    public static void main(String[] args) 
+    {
+    //      StringBuffer nm=new StringBuffer();
+    //      System.out.println(nm.length());
+    //      System.out.println(nm.capacity());
+    //      nm.append("abcdefghijklmnop");
+    //      System.out.println(nm.capacity());
+    //      System.out.println(nm.length());
+    //      nm.append("Q");
+    //      System.out.println(nm.capacity());
+    //      System.out.println(nm.length());
+    
+    // StringBuffer nm=new StringBuffer("Sachin");
+    // System.out.println(nm);
+    // System.out.println(nm.capacity());
+    // nm.setCharAt(1,'A');
+    // System.out.println(nm);
+    // System.out.println(nm.charAt(1));
+     
+    // StringBuffer nm=new StringBuffer(150);
+    // System.out.println(nm.capacity());
+    // nm.append("Sachin");
+    // System.out.println(nm.capacity());
+    // nm.trimToSize();
+    // System.out.println(nm.capacity());
+
+    StringBuilder nm=new StringBuilder("pwskills");
+    System.out.println(nm);
+    System.out.println(nm.reverse());
+    
+    
+}
+}
