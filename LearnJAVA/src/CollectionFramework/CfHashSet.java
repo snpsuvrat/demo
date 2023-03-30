32d82d7891fdf0c2bf6aa488502f544af5d42043
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-03-31 00:37:59 +0530

    HAshSet

---
+++ LearnJAVA/src/CollectionFramework/CfHashSet.java
@@ -0,0 +1,15 @@
+package CollectionFramework;
+import java.util.*;
+
+public class CfHashSet
+{
+    public static void main(String[] args) 
+    {
+        HashSet hs=new HashSet();
+        hs.add(100);
+        hs.add(20);
+        hs.add(30);
+        hs.add(40);
+        System.out.println(hs);
+    }
+}
--- LearnJAVA/src/CollectionFramework/CfTreeSet.java
+++ LearnJAVA/src/CollectionFramework/CfTreeSet.java
@@ -6,8 +6,28 @@ public class CfTreeSet
 {
     public static void main(String[] args) 
     {
-        TreeSet ts=new TreeSet();
+        TreeSet ts=new TreeSet();  // it follows binary search tree
         ts.add(100);
+        ts.add(50);
+        ts.add(150);
+        ts.add(25);
+        ts.add(75);
+        ts.add(125);
+        ts.add(175);
+
+        System.out.println(ts);
+        ts.add(100); //duplicates are not considereed
+        System.out.println(ts);
+        System.out.println(ts.higher(50));
+        System.out.println(ts.lower(50));
+
+        System.out.println(ts.higher(40));
+        System.out.println(ts.lower(40));
+
+        System.out.println(ts.ceiling(40));
+        System.out.println(ts.floor(40));
       
+        System.out.println(ts.ceiling(50));
+        System.out.println(ts.floor(50));
     }
 }
