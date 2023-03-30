ad42e0c82492c5dff3fae28469d60d519749f80d
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-03-31 00:16:34 +0530

    CollectionFramework

---
+++ LearnJAVA/src/CollectionFramework/CfTreeSet.java
@@ -0,0 +1,13 @@
+package CollectionFramework;
+
+import java.util.TreeSet;
+
+public class CfTreeSet 
+{
+    public static void main(String[] args) 
+    {
+        TreeSet ts=new TreeSet();
+        ts.add(100);
+      
+    }
+}
