e053159cddd5fc80e4d5f16f8ca5ab3a7719725a
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-05-22 12:01:42 +0530

    HELLO

--- LearnJAVA/src/ALLcode/PatternK.java
+++ LearnJAVA/src/ALLcode/PatternK.java
@@ -2,7 +2,7 @@ public class PatternK
 {
     public static void main(String []args)
     {
-        int n=19;
+        int n=16;
         for(int i=0;i<n;i++)
         {
             for(int j=0; j<n; j++)
