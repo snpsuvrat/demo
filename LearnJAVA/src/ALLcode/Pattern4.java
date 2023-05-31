9982732f2b0b194874cb3f0559387e05585aa8ba
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-05-31 19:42:17 +0530

    Hello

--- LearnJAVA/src/ALLcode/Pattern4.java
+++ LearnJAVA/src/ALLcode/Pattern4.java
@@ -3,7 +3,7 @@ public class Pattern4
 {
     public static void main(String []args)
     {
-        int n=11;
+        int n=12;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
