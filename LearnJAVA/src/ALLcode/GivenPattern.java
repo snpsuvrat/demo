7a90c6607dd3a3c6186d560a25aaca7500870d56
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-06-12 20:45:48 +0530

    hello

--- LearnJAVA/src/ALLcode/GivenPattern.java
+++ LearnJAVA/src/ALLcode/GivenPattern.java
@@ -3,7 +3,7 @@ public class GivenPattern
 {
     public static void main(String []args)
     {
-        int n=13;
+        int n=14;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
